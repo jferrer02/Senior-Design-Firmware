56abd91cde2cb276a74ad5ee1b6583ff91d767f7
Joshua Ferrer <ferrer.joshua@gmail.com>
2023-02-23 01:05:30 -0800

    now working on a menu

--- main.ino
+++ main.ino
@@ -14,7 +14,9 @@ int b_2 = 0;
 int X = 0;  // variable to store the value read
 int Y = 0;  // variable to store the value read
 
-//menu options
+//menu variables
+int menu_val = 0;
+int menu_sel = 0;
 
 // use this line for single matrix
 Adafruit_HT1632LEDMatrix matrix = Adafruit_HT1632LEDMatrix(HT_DATA, HT_WR, HT_CS);
@@ -34,78 +36,62 @@ void loop() {
   matrix.setRotation(0);
   matrix.clearScreen();
   matrix.setCursor(0, 0);
-  
-  //menu function
-  
   b_1 = digitalRead(button_1);
   b_2 = digitalRead(button_2);
   X = analogRead(A0);  // read the A0 input pin
   Y = analogRead(A1);  // read the A1 input pin 
-
-  if (b_1 == LOW) {
-    matrix.print("b1");
-  }       
-  else if ((X > 550) || (X < 450)) {
-    matrix.print(X);
-  }
-  else {
-    matrix.print("none");
-  }
-
-  matrix.print("\n");
-  
-  if (b_2 == LOW) {
-    matrix.print("b2");
-  }
-  else if ((Y > 550) || (Y < 450)) {
-    matrix.print(Y); 
-  }
-  else {
-    matrix.print("none");
+  if (X > 550) {
+    menu_val++;
+    if (menu_val > 1) {
+      menu_val = 0;
     }
+  }
+  if (X < 450) {
+    menu_val--;
+    if (menu_val < 0) {
+      menu_val = 1;
+    }
+  }
+
+
   matrix.writeScreen();
   delay(50);
-  
-  /*
-  matrix.print("New \n" "test"); 
-  matrix.writeScreen(); 
-  delay(4000);
-  for(uint8_t i=0; i<4; i++) {
+}
+
+void testcomponents() {
+  int i = 0;
+  while (i < 1) {
     matrix.setRotation(0);
     matrix.clearScreen();
     matrix.setCursor(0, 0);
-    matrix.print("Test");
-    matrix.writeScreen();
-    delay(1000);
-    for (uint8_t y=0; y<matrix.height(); y++) {
-      for (uint8_t x=0; x< matrix.width(); x++) {
-        matrix.setPixel(x, y);
-        matrix.writeScreen();
-      }
+  
+    b_1 = digitalRead(button_1);
+    b_2 = digitalRead(button_2);
+    X = analogRead(A0);  // read the A0 input pin
+    Y = analogRead(A1);  // read the A1 input pin 
+
+    if (b_1 == LOW) {
+      matrix.print("b1");
+    }       
+    else if ((X > 550) || (X < 450)) {
+      matrix.print(X);
     }
-    // blink!
-    matrix.blink(false);
-    delay(2000);
-    matrix.blink(false);
-    // Adjust the brightness down
-    for (int8_t i=15; i>=0; i--) {
-      matrix.setBrightness(i);
-      delay(100);
-    }
-    // then back up
-    for (uint8_t i=0; i<16; i++) {
-      matrix.setBrightness(i);
-      delay(100);
+    else {
+      matrix.print("none");
     }
 
-    for (uint8_t y=0; y<matrix.height(); y++) {
-      for (uint8_t x=0; x< matrix.width(); x++) {
-        matrix.clrPixel(x, y);
-        matrix.writeScreen();
-      
-      }
-      
+    matrix.print("\n");
+  
+    if (b_2 == LOW) {
+      matrix.print("b2");
     }
+    else if ((Y > 550) || (Y < 450)) {
+      matrix.print(Y); 
+    }
+    else {
+      matrix.print("none");
+      }
+    matrix.writeScreen();
+    delay(50);
   }
-  */
 }
