691b0182fa4b2baf8342052e3e6c234046474d4f
Joshua Ferrer <ferrer.joshua@gmail.com>
2023-03-09 14:30:05 -0800

    Added more to Game1 - working on arrows that cross the screen.

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 #include "Adafruit_HT1632.h"
 #define HT_DATA 3
 #define HT_WR   4
@@ -32,8 +34,8 @@ void setup() {
 }
 
 void testcomponents();
-
 void Game1();
+void OutputArrows();
 
 void loop() {
   matrix.setRotation(0);
@@ -138,20 +140,20 @@ void Game1() {
   int health_bar = 1; //players health, lose if reaches 0
   // arrow pointing to the right
   int right_point_y = rand()%(13-2 + 1) + 0;
-  std::array<int, 5> right_arrow_x = {-2,-1,0,-1,-2};
-  std::array<int, 5> right_arrow_y = {right_point_y+2,right_point_y+1,right_point_y,right_point_y-1,right_point_y-2};
+  int ra_x[5] = {-2,-1,0,-1,-2};
+  int ra_y[5] = {right_point_y+2,right_point_y+1,right_point_y,right_point_y-1,right_point_y-2};
   // arrow pointing to the left
   int left_point_y = rand()%(13-2 + 1) + 0;
-  std::array<int, 5> left_arrow_x = {25,24,23,24,25};
-  std::array<int, 5> left_arrow_y = {left_point_y+2,left_point_y+1,left_point_y,left_point_y-1,left_point_y-2};
+  int la_x[5] = {25,24,23,24,25};
+  int la_y[5] = {left_point_y+2,left_point_y+1,left_point_y,left_point_y-1,left_point_y-2};
   // arrow pointing up
   int up_point_x = rand()%(21-2 + 1) + 0;
-  std::array<int, 5> up_arrow_x = {up_point-2,up_point-1,up_point_x,up_point+1,up_point+2};
-  std::array<int, 5> up_arrow_y = {17,16,15,16,17};
+  int ua_x[5] = {up_point-2,up_point-1,up_point_x,up_point+1,up_point+2};
+  int ua_y[5] = {17,16,15,16,17};
   // arrow pointing down
   int down_point_x = rand()%(21-2 + 1) + 0;
-  std::array<int, 5> down_arrow_x = {down_point-2,down_point-1,down_point_x,down_point+1,down_point+2};
-  std::array<int, 5> down_arrow_y = {-2,-1,0,-1,-2};
+  int da_x[5] = {down_point-2,down_point-1,down_point_x,down_point+1,down_point+2};
+  int da_y[5] = {-2,-1,0,-1,-2};
   matrix.setRotation(0);
   matrix.clearScreen();
   matrix.setCursor(0, 0); 
@@ -218,10 +220,48 @@ void Game1() {
       y = 15;
     } 
     matrix.setPixel(x, y);
+
+    //right pointing arrow
+    OutputArrows(ra_x[0],ra_x[1],ra_x[2],ra_x[3],ra_x[4],ra_y[0],ra_y[1],ra_y[2],ra_y[3],ra_y[4]);
+    if ((x==ra_x[0] && y==ra_y[0]) || (x==ra_x[1] && y==ra_y[1]) || (x==ra_x[2] && y==ra_y[2]) || (x==ra_x[3] && y==ra_y[3]) || (x==ra_x[4] && y==ra_y[4])) {
+      health_bar--;
+      matrix.setRotation(0);
+      matrix.print("Hlth\n");
+      matrix.print(health_bar);
+      matrix.writeScreen();
+      delay(2000);
+      matrix.clearScreen();
+      healthdot_countdown = 100;
+      //taking code from stack overflow
+      //https://stackoverflow.com/questions/12657962/how-do-i-generate-a-random-number-between-two-variables-that-i-have-stored
+      healthdot_x = rand()%(23-0 + 1) + 0;
+      healthdot_y = rand()%(15-0 + 1) + 0;
+      /* reset x and y values of arrows, can use references in a function that also take in
+      parameters for how far back the arrows can start (where there will be some randomness).
+      The more health you have, the less far back the arrows can start and the game gets more
+      difficult.
+      */
+    }
+    ra_x[0] = ra_x[0] + 1; //increment arrow
+    ra_x[1] = ra_x[1] + 1; //increment arrow
+    ra_x[2] = ra_x[2] + 1; //increment arrow
+    ra_x[3] = ra_x[3] + 1; //increment arrow
+    ra_x[4] = ra_x[4] + 1; //increment arrow
+
+
+
     matrix.writeScreen();
     if (b_1 == LOW) {
       break;
     }
     delay(50); 
   }  
+}
+
+OutputArrows(x0,x1,x2,x3,x4,y0,y1,y2,y3,y4) {
+  matrix.setPixels(x0,y0);
+  matrix.setPixels(x1,y1);
+  matrix.setPixels(x2,y2);
+  matrix.setPixels(x3,y3);
+  matrix.setPixels(x4,y4);
 }
